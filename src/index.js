732ac83e278c758a7469a2d4f23a2c2bd9d16fbf
mrkamin <108926875+mrkamin@users.noreply.github.com>
2022-11-19 20:22:58 +0500

    Add Function for Add or Remove

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import './index.css';
-import { DateTime } from './modules/luxon.js';
 /* eslint-disable import/no-cycle */
 import { showToDoTasks } from './modules/task-show.js';
 
-const time = document.getElementById('time');
+
 // Initial References
 export const inputNewTasks = document.querySelector('#sect__one__cont__items__item__one input');
 
-const now = DateTime.now();
-time.innerHTML = `Date:[${now.day}-${now.month}-${now.year}] | Time:[${now.hour}:${now.minute}:${now.second}]`;
 // Disable Edit Button
 export const buttonsDeactive = (bool) => {
   const buttonModify = document.getElementsByClassName('edit__btn');
